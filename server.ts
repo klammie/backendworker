aab38a91f1cfda8231a16f26b47c12236dcbeb49
klammie <karrellelammie@gmail.com>
2025-05-26 15:01:35 -0500

    edit api

--- server.ts
+++ server.ts
@@ -53,7 +53,7 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.send("Welcome to Corefunction API");
+  res.status(200).send("Welcome to Corefunction API");
 });
 
 // ✅ Graceful shutdown handling
